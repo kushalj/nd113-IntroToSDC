ec0343f26c85643853628caac1bde83b9d428826
Kushal Joshi <kushal@joshi.com>
2017-10-11 14:35:09 +0100

    added go clean to run file

--- run.sh
+++ run.sh
@@ -1,21 +1,23 @@
-echo "python $1.py:"
+echo
+echo -e "\e[32mpython $1.py:\e[39m"
 [ -f "$1.py" ] && python $1.py || echo "The file does not exist"
 echo
 
 echo
-echo "julia $1.jl"
+echo -e "\e[35mjulia $1.jl\e[39m"
 [ -f "$1.jl" ] && julia $1.jl || echo "The file does not exist"
 
 echo
 
 echo
-echo "go build $1.go"
+echo -e "\e[34mgo build $1.go\e[39m"
+go clean
 [ -f "$1.go" ] && go build $1.go || echo "The file does not exist"
 [ -f "./$1" ] && ./$1 || echo "No compiled file"
 echo
 
 echo
-echo "g++ $1.cpp"
+echo -e "\e[33mg++ $1.cpp\e[39m"
 [ -f "$1.cpp" ] && g++ $1.cpp || echo "The file does not exist"
 [ -f "./a.out" ] && ./a.out || echo "No compiled file"
 echo
