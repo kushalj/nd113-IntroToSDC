d1a400174bd65ea759b567ddf4550f7bbab02c1c
Kushal Joshi <kushal@joshi.com>
2017-11-16 12:48:12 +0000

    renamed probability folder

--- bayesianThinking/moveTwice.py
+++ bayesianThinking/moveTwice.py
@@ -1,6 +1,6 @@
-#Modify the move function to accommodate the added
-#probabilities of overshooting or undershooting
-#the intended destination.
+#Write code that makes the robot move twice and then prints
+#out the resulting distribution, starting with the initial
+#distribution p = [0, 1, 0, 0, 0]
 
 p=[0, 1, 0, 0, 0]
 world=['green', 'red', 'red', 'green', 'green']
@@ -23,14 +23,17 @@ def sense(p, Z):
 
 def move(p, U):
     q = []
-    # move first...
     for i in range(len(p)):
-        cell = pExact * p[(i-U)%len(p)]
-        cell = cell + pOvershoot * p[(i-U+1)%len(p)]
-        cell = cell + pUndershoot * p[(i-U-1)%len(p)]
-        q.append(cell)
-
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
     return q
+#
+# ADD CODE HERE
+#
+moves = 1000
+for m in range(moves):
+    p = move(p, 1)
 
-
-print(move(p, 0))
+print(p)
